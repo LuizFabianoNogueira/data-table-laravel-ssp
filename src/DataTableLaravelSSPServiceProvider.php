e7fbd3790fc746e0515c3434fcc672274dbc97fb
Luiz Fabiano Nogueira <luiz-fabiano.nogueira.ext@bureauveritas.com>
2024-10-17 19:48:43 -0300

    data-table css

--- src/DataTableLaravelSSPServiceProvider.php
+++ src/DataTableLaravelSSPServiceProvider.php
@@ -2,32 +2,16 @@
 
 namespace LuizFabianoNogueira\DataTableLaravelSSP;
 
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class DataTableLaravelSSPServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        Blade::directive('dtl', static function ($expression) {
-            return "<?php echo 'Executando a diretiva @algo com: ' . $expression; ?>";
-        });
-
         $this->loadRoutesFrom(__DIR__ . '/Routes/dtl.php');
-        $this->loadMigrationsFrom(__DIR__ . '/Migrations');
-
-        $this->publishesMigrations([
-            __DIR__.'/Migrations' => database_path('migrations'),
-        ], 'data-table-laravel-migrations');
 
         $this->publishes([
             __DIR__.'/Config/dtl.php' => config_path(),
         ], 'data-table-laravel-config');
-
-        $this->publishes([
-            __DIR__.'/Config/dtl.php' => config_path(),
-        ], 'data-table-laravel-js');
-
-
     }
 }
--- src/Routes/dtl.php
+++ src/Routes/dtl.php
@@ -6,3 +6,13 @@ use LuizFabianoNogueira\DataTableLaravelSSP\Http\Controllers\DtlController;
 Route::controller(DtlController::class)->prefix('dtl')->group(function () {
     Route::get('list', 'list')->name('dtl.dynamic.list');
 });
+
+Route::get('/data-table-laravel-ssp/js', static function () {
+    $path = __DIR__ . '../../Assets/Js/dtl.js';
+    return response()->file($path, ['Content-Type' => 'application/javascript']);
+})->name('dtl.js');
+
+Route::get('/data-table-laravel-ssp/css', static function () {
+    $path = __DIR__ . '../../Assets/Css/dtl.css';
+    return response()->file($path, ['Content-Type' => 'text/css']);
+})->name('dtl.css');
